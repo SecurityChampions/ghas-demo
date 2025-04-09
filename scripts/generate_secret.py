843310e8702bb399d62aa92d37f235165aa8362b
Sec-champ <Mindtreecybersec3@gmail.com>
2025-04-09 12:14:50 +0530

    Update generate_secret.py

--- scripts/generate_secret.py
+++ scripts/generate_secret.py
@@ -2,5 +2,6 @@
 import rstr
 
 print(f"AWS_KEYID: {rstr.xeger(r'AKIA[A-Z2-7]{16}')}")
+AWS_KEYID:AKIAOE66RCI5C4DUTRD2
 print(f"AWS_SECRET: {rstr.xeger(r'[0-9A-Za-z/+=]{40}')}")
 print(f"DATADOG_TOKEN: {rstr.xeger(r'[a-f0-9]{32}|[a-f0-9]{40}')}")
